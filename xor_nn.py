7e301b5539d837f999e584d09e273a1230ce41a6
Erik Peteri <peteri.erik@gmail.com>
2025-06-22 14:45:12 +0300

    First commit

---
+++ xor_nn.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class XorNet:
+    def __init__(self):
+        pass
+
+    def hello_world(self):
+        return "Hello world!"
+
+model = XorNet()
+print(model.hello_world())
