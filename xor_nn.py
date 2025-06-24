c214af487ed9c15d13713373527e74c324054727
Erik Peteri <peteri.erik@gmail.com>
2025-06-24 13:50:35 +0300

    Implement forward propagation

--- xor_nn.py
+++ xor_nn.py
@@ -7,14 +7,21 @@ class XorNet:
 
         self._weights_output = np.random.rand(1, 2)
         self._bias_output = np.random.rand(1)
-        # TODO
+
+        self._hidden_layer = None
+        self._output_layer = None
 
     def _relu(self, x):
         return np.maximum(0, x)
 
-    def _forward_propagation(self):
-        # TODO
-        pass
+    def _forward_propagation(self, x):
+        z_hidden = np.dot(self._weights_hidden, x) + self._bias_hidden
+        self._hidden_layer = self._relu(z_hidden)
+
+        z_output = np.dot(self._weights_output, self._hidden_layer) + self._bias_output
+        self._output_layer = self._relu(z_output)
+    
+        return self._output_layer
 
     def _backpropagation(self):
         # TODO
@@ -22,4 +29,8 @@ class XorNet:
 
     def train_step(self):
         # TODO
+        pass
+
+    def predict(self):
+        # TODO 
         pass
