a4caa0bf84fe1af285847f4c8c423e5a05340259
Erik Peteri <peteri.erik@gmail.com>
2025-06-24 14:48:04 +0300

    Fix dot product logic in forward propagation to obtain output for all input pairs

--- train.py
+++ train.py
@@ -6,8 +6,10 @@ def main():
     training_data = np.array([[0, 0], [1, 1],
                             [0, 1], [1, 0]])
     model = XorNet()
-    visualizer = Visualizer()
     
+    prediction = model.predict(training_data)
+    print(prediction)
+
 if __name__ == "__main__":
     main()
 
--- xor_nn.py
+++ xor_nn.py
@@ -2,35 +2,35 @@ import numpy as np
 
 class XorNet:
     def __init__(self):
-        self._weights_hidden = np.random.rand(2, 2)
-        self._bias_hidden = np.random.rand(2)
+        self._weights_hidden = np.array([[1, 1], [1, 1]])
+        self._bias_hidden = np.array([0, -1])
 
-        self._weights_output = np.random.rand(1, 2)
-        self._bias_output = np.random.rand(1)
+        self._weights_output = np.array([[1, -2]])
+        self._bias_output = np.array([0])
 
-        self._hidden_layer = None
-        self._output_layer = None
+        self._hidden_layer_output = None
+        self._output_layer_output = None
 
-    def _relu(self, x):
+    def _relu(self, x: np.ndarray):
         return np.maximum(0, x)
 
-    def _forward_propagation(self, x):
-        z_hidden = np.dot(self._weights_hidden, x) + self._bias_hidden
-        self._hidden_layer = self._relu(z_hidden)
+    def _forward_propagation(self, x: np.ndarray):
+        z_hidden = np.dot(x, self._weights_hidden.T) + self._bias_hidden
+        self._hidden_layer_output = self._relu(z_hidden)
 
-        z_output = np.dot(self._weights_output, self._hidden_layer) + self._bias_output
-        self._output_layer = self._relu(z_output)
+        z_output = np.dot(self._hidden_layer_output, self._weights_output.T) + self._bias_output
+        self._output_layer_output = self._relu(z_output)
     
-        return self._output_layer
+        return self._output_layer_output
 
     def _backpropagation(self):
         # TODO
         pass
 
-    def train_step(self):
-        # TODO
-        pass
+    def train_step(self, x: np.ndarray):
+        output = self._forward_propagation(x)
+        return output
 
-    def predict(self):
-        # TODO 
-        pass
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        prediction = self._forward_propagation(x)
+        return prediction
