309db69db392160c8a03b972e96a5621deee15a3
Erik Peteri <peteri.erik@gmail.com>
2025-07-02 21:47:51 +0300

    Refactor neural net to use classes defined in activations.py and costs.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from activations import Sigmoid, ReLU
+from costs import MSE, BCE
 from xor_nn import XorNet
 from visualizer import Visualizer
 
@@ -6,11 +8,11 @@ def main():
     training_data = np.array([[0, 0], [1, 0],[0, 1], [1, 1]])
     labels = np.array([[0], [1], [1], [0]])
 
-    model = XorNet()
+    model = XorNet(Sigmoid(), BCE())
     visualizer = Visualizer()
 
     costs = []
-    for _ in range(10000):
+    for _ in range(100000):
         output, cost = model.train_step(training_data, labels)
         costs.append(cost)
     print(model.predict(training_data))
--- xor_nn.py
+++ xor_nn.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 class XorNet:
-    def __init__(self):
+    def __init__(self, activation, cost):
+        self._activation = activation
+        self._cost = cost
+
         self._x = None
 
+        # Weights and biases
         self._w_hidden = np.random.rand(2, 2) 
         self._b_hidden = np.random.rand(2)
-
         self._w_output = np.random.rand(1, 2) 
         self._b_output = np.random.rand(1)
 
@@ -19,20 +22,17 @@ class XorNet:
     def _forward_propagation(self, x: np.ndarray):
         self._x = x
         self._z_hidden = np.dot(x, self._w_hidden.T) + self._b_hidden
-        self._h = self._sigmoid(self._z_hidden)
-        #self._h = self._relu(self._z_hidden)
+        self._h = self._activation(self._z_hidden)
 
         self._z_output = np.dot(self._h, self._w_output.T) + self._b_output
-        self._o = self._sigmoid(self._z_output)
-        #self._o = self._relu(self._z_output)
+        self._o = self._activation(self._z_output)
     
         return self._o
 
     def _backpropagation(self, y_true):
         # Gradients for the output layer
-        dc_do = self._bce_prime(y_true) # ∂c/∂o
-        do_dz = self._sigmoid_prime(self._z_output) # ∂o/∂z
-        #do_dz = self._relu_prime(self._z_output) # ∂o/∂z
+        dc_do = self._cost.derivative(y_true, self._o) # ∂c/∂o
+        do_dz = self._activation.derivative(self._z_output) # ∂o/∂z
         dc_dz = dc_do * do_dz # ∂c/∂o · ∂o/∂z
 
         dc_dw = np.dot(dc_dz.T, self._h) # ∂c/∂w = ∂c/∂o · ∂o/∂z · ∂z/∂w 
@@ -40,41 +40,13 @@ class XorNet:
 
         # Gradients for the hidden layer
         dc_dh = np.dot(dc_dz, self._w_output)
-        dh_dz_hidden = self._sigmoid_prime(self._z_hidden)
-        #dh_dz_hidden = self._relu_prime(self._z_hidden)
+        dh_dz_hidden = self._activation.derivative(self._z_hidden)
         dc_dz_hidden = dc_dh * dh_dz_hidden
         
         dc_dw_hidden = np.dot(dc_dz_hidden.T, self._x)
         dc_db_hidden = np.sum(dc_dz_hidden, axis=0)      
 
         self._update_weights(dc_dw, dc_db, dc_dw_hidden, dc_db_hidden)
-
-    def _sigmoid(self, x: np.ndarray):
-        return 1 / (1 + np.exp(-x))
-    
-    def _sigmoid_prime(self, x):
-        s = self._sigmoid(x)
-        return s * (1 - s)
-
-    def _relu(self, x: np.ndarray):
-        return np.maximum(0, x)
-
-    def _relu_prime(self, x: np.ndarray):
-        return (x > 0).astype(float)
-
-    def _cost_function(self, y_true: np.ndarray):
-        cost = np.mean((y_true - self._o) ** 2)
-        return cost
-    
-    def _cost_function_prime(self, y_true: np.ndarray):
-        cost_prime = 2 * (self._o - y_true) / y_true.shape[0]
-        return cost_prime
-
-    def bce(self, y_true):
-        return -np.mean(y_true * np.log(self._o) + (1 - y_true) * np.log(1 - self._o))
-
-    def _bce_prime(self, y_true: np.ndarray):
-        return self._o - y_true 
 
     def _update_weights(self, dc_dw, dc_db, dc_dw_hidden, dc_db_hidden, step_size = 0.1):
         self._w_output -= step_size * dc_dw
@@ -85,7 +57,7 @@ class XorNet:
     def train_step(self, x: np.ndarray, y_true: np.ndarray):
         output = self._forward_propagation(x)
         self._backpropagation(y_true)  
-        cost = self.bce(y_true)
+        cost = self._cost(y_true, self._o)
         return (output, cost)
 
     def predict(self, x: np.ndarray) -> np.ndarray:
